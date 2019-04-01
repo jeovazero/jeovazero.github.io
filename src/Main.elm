65d66e3e563f4ba943331b5b51d6ae0f303c33ee
jeovazero <jeovapereira2000@live.com>
2019-04-01 02:06:17 -0300

    feat: initial home

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,7 @@
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 
 main =
   Browser.element
@@ -10,10 +11,30 @@ main =
     , view = view
     }
 
+brandIcon : String -> String -> Html msg
+brandIcon name link =
+    a [ href link ]
+      [ i [ class (String.concat ["fab ", name]) ] []
+      ]
+
 view model =
     div [ class "wrapper" ]
-        [ div [ class "logo" ]
-            [ text "jeovazero",
-            text "Desenvolvedor JavaScript"
-            ]
+        [ div [ class "main" ]
+              [ img [ src "./assets/logo.png" ] []
+              , div [ class "desc"]
+                    [ text "Desenvolvedor JavaScript"
+                    , br [] []
+                    , text "Back-end/Front-end"
+                    ]
+              , div [ class "bio" ] [ text "Bio" ]
+              , div [ class "contacts" ]
+                    [ brandIcon "fa-telegram" "https://t.me/jeotario"
+                    , brandIcon "fa-github" "https://github.com/jeovazero"
+                    , brandIcon "fa-twitter" "https://twitter.com/jeovazero"
+                    , brandIcon "fa-linkedin" "https://linkedin.com/in/jeovazero"
+                    ]
+              ]
+        , div [ class "status" ] [ text "Construindo com ELM <3"]
+        , footer []
+                 [ span [] [ text "Copyright (c) 2019 jeovazero" ] ]
         ]
