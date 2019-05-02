595ead5aae143f5ac9837adb701d02b35e4b3adf
jeovazero <jeovapereira2000@live.com>
2019-05-02 19:33:14 -0300

    style: add assets

--- src/Projects.elm
+++ src/Projects.elm
@@ -13,6 +13,7 @@ type alias ProjectInfo =
   { name: String
   , tags: List String
   , description: String
+  , imgsrc: String
   , projectLink: Link
   , githubLink: Link
   }
@@ -34,20 +35,31 @@ projectItem
     { name
     , tags
     , description
+    , imgsrc
     , projectLink
     , githubLink} =
         div [ class "project-item" ]
-        [ div [ class "project-item-title" ]
-              [ div []
-                    [ linkView projectLink name ] ]
-        , div [ class "project-item-description" ]
-              [ text description ]
-        , div [ class "project-item-tags" ]
-              (List.map (\x -> tagView x) tags)
-        , div [ class "project-item-link" ]
-              [ linkView projectLink "Projeto"
-              , linkView githubLink "Github" ]
+        [ a (linkResolve projectLink ++ [ class "project-item-img", style "background-image" imgsrc ]) []
+        , div   [ class "project-item-content" ]
+                [ div [ class "project-item-title" ]
+                      [ div []
+                            [ linkView projectLink name ] ]
+                , div   [ class "project-item-description" ]
+                        [ text description ]
+                , div   [ class "project-item-tags" ]
+                        (List.map (\x -> tagView x) tags)
+                , div   [ class "project-item-link" ]
+                        [ text "Links -> "
+                        , linkView projectLink "Projeto"
+                        , linkView githubLink "Github" ]
+                ]
         ]
+
+linkResolve link =
+    case link of
+        NoLink -> []
+        Link lnk -> [ href lnk, target "_blank" ]
+
 
 linkView link label =
     case link of
@@ -61,27 +73,38 @@ tagView tag =
 
 projectData =
     [ ProjectInfo
-        "8 Puzzle React"
+        "8 Puzzle React [WEB APP]"
         ["react", "A*", "busca", "react-spring", "redux"]
         "O jogo dos 8 feito com React e Redux"
+        "url(../assets/8puzzle.png)"
         (Link "https://8-puzzle-react.jeova.ninja/")
         (Link "https://github.com/jeovazero/8-puzzle-react")
     , ProjectInfo
-        "Opacity Project Front-end"
+        "Opacity Project [WEB APP]"
+        ["react", "emotionjs", "flow", "webpack"]
+        "Um mini-portal que usa dados do \"Portal da Transparências\""
+        "url(../assets/opacity.png)"
+        (Link "http://opacity-project.netlify.com")
+        (Link "https://github.com/AkatsukiJS/opacity-project-front-end")
+    , ProjectInfo
+        "Opacity Project [Storybook]"
         ["storybook", "react", "emotionjs", "flow", "webpack"]
         "Uma coleção de componentes para Opacity-Project"
+        "url(../assets/opacity-story.png)"
         (Link "http://opacity-storybook.surge.sh")
         (Link "https://github.com/AkatsukiJS/opacity-project-front-end")
     , ProjectInfo
-        "VUTTR Front-end"
+        "VUTTR [Storybook]"
         ["storybook", "react", "emotionjs", "flow", "webpack"]
         "Uma coleção de componentes para a aplicação VUTTR"
+        "url(../assets/vuttr-story.png)"
         (Link "http://vuttr-ds.surge.sh/")
         (Link "https://github.com/jeovazero/vuttr-frontend")
     , ProjectInfo
-        "Adviceme"
+        "Adviceme [WIP]"
         ["vuejs", "sass", "webpack4", "postcss"]
         "Um site fictício para auxiliar advogados iniciantes com ênfase no Piauí"
+        "url(../assets/adviceme.png)"
         (Link "http://app.jeova.ninja/")
         NoLink
     ]
